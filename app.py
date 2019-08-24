8f9dcbd226c654b3f5fdc330bd3a4bb6deea7ab3
Heiner <hacm1997@gmail.com>
2019-08-23 22:19:28 -0500

    doc

--- app.py
+++ app.py
@@ -3,14 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 import os
 
+#Dirección de la Bd - Aún no configurada correctamente
 dbdir = "sqlite:///" + os.path.abspath(os.getcwd()) + "database/users.sql"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
 db = SQLAlchemy(app)
 
+#Primera ruta 'index.html'
 @app.route("/")
 def index():
     return render_template("index.html")
 
+#Ruta para ver el listado de usuarios
 @app.route("/listado.html")
